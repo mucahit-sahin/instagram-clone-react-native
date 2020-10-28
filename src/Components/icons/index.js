90881ebb31b0b3b4b508b7e056446c48281c9053
mucahit-sahin <mucahitsahin786@gmail.com>
2020-10-28 14:13:08 +0300

    Bookmark iconu eklenip , componente dönüştürüldü

--- src/Components/icons/index.js
+++ src/Components/icons/index.js
@@ -1,3 +1,4 @@
+export { default as Bookmark } from './Bookmark'
 export { default as Camera } from './Camera'
 export { default as Comment } from './Comment'
 export { default as Heart } from './Heart'
