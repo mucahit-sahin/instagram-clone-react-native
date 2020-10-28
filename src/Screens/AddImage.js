808cb36dcd873b39fe2783fa99c7d9829a52cb93
mucahit-sahin <mucahitsahin786@gmail.com>
2020-10-28 16:46:43 +0300

    AddImage ekranının ilk oluşturulması

---
+++ src/Screens/AddImage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AddImage = () => {
+  return (
+    <View>
+      <Text>add image</Text>
+    </View>
+  );
+};
+
+export default AddImage;
+
+const styles = StyleSheet.create({});
