8a5915e5f2ac35d80298899d67cf910de03f9ce6
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-01 17:19:37 +0300

    Profiledeki header tasarımı oluşturuldu

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {Accounts, Menu} from '../Components/icons';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerText}>username</Text>
+        </View>
+        <Accounts />
+        <View style={styles.headerMenu}>
+          <Menu />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fafafa',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTextView: {
+    paddingBottom: 10,
+    paddingTop: 10,
+    paddingRight: 10,
+  },
+  headerText: {
+    fontWeight: 'bold',
+  },
+  headerMenu: {position: 'absolute', right: 10},
+});
