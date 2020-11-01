419a20a6016bed7a9c35c775b5ed39d26feb17d6
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-01 20:17:20 +0300

    profile biyografi tasarımı yapıldı

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import {Accounts, Menu} from '../Components/icons';
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
 const Profile = () => {
   return (
     <View style={styles.container}>
@@ -14,6 +17,28 @@ const Profile = () => {
           <Menu />
         </View>
       </View>
+      <View style={styles.profileBiyografi}>
+        <View style={styles.profileImage}></View>
+        <View style={styles.postsizes}>
+          <Text style={styles.size}>15</Text>
+          <Text style={styles.sizename}>Posts</Text>
+        </View>
+        <View style={styles.postsizes}>
+          <Text style={styles.size}>105</Text>
+          <Text style={styles.sizename}>Follows</Text>
+        </View>
+        <View style={styles.postsizes}>
+          <Text style={styles.size}>121</Text>
+          <Text style={styles.sizename}>Followers</Text>
+        </View>
+      </View>
+      <View style={styles.biyografiText}>
+        <Text style={styles.displayName}>Display Name</Text>
+        <Text>Biyografi</Text>
+      </View>
+      <View style={styles.editProfileButton}>
+        <Text>Edit Profile</Text>
+      </View>
     </View>
   );
 };
@@ -23,6 +48,8 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fafafa',
+    width: width,
+    height: height,
   },
   header: {
     flexDirection: 'row',
@@ -37,5 +64,40 @@ const styles = StyleSheet.create({
   headerText: {
     fontWeight: 'bold',
   },
-  headerMenu: {position: 'absolute', right: 10},
+  headerMenu: {position: 'absolute', right: 20},
+  profileBiyografi: {
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  profileImage: {
+    backgroundColor: 'red',
+    width: 75,
+    height: 75,
+    borderRadius: 37.5,
+    marginRight: 30,
+  },
+  postsizes: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginRight: 20,
+    flex: 0.33,
+  },
+  size: {fontWeight: 'bold'},
+  sizename: {color: 'gray'},
+  biyografiText: {marginLeft: 20, marginTop: 20},
+  displayName: {fontWeight: 'bold'},
+  editProfileButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    height: 40,
+    marginRight: 10,
+    marginLeft: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
 });
